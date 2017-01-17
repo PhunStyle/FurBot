3ea9fdadf68ba4ee5da27b394cf3313cb3885b6b
PhunStyle <jager1993@gmail.com>
2017-01-17 11:55:13 +0100

    Try to find the cause

--- src/discord.js
+++ src/discord.js
@@ -9,6 +9,9 @@ import logger from './logger';
 import sentry from './sentry';
 import startExpress from './express';
 import { init as initPhantom } from './phantom';
+import SegfaultHandler from 'segfault-handler';
+
+SegfaultHandler.registerHandler("crash.log");
 
 import { startPortalTimeouts, startPortalIntervals } from './portals';
 import {
