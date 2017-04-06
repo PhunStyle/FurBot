476b5de7950c8ba6e74d1303401172d38ba6b67c
Johan <jager1993@gmail.com>
2017-04-06 14:38:41 +0200

    Branch 3.8.0 initial changes

---
+++ src/commands/actions/hug.js
@@ -0,0 +1,43 @@
+import Promise from 'bluebird';
+
+
+function hug(client, evt) {
+  let receiverArray = [];
+
+  if (evt.message.mentions.length !== 0) {
+    evt.message.mentions.map(user => {
+      receiverArray.push(user.mention);
+    });
+
+    let receivers = receiverArray.join(' and ');
+
+    const hugs = [
+      `jumps on ${receivers} and hugs them tight! :hugging:`,
+      `walks up to ${receivers} and gives them a hug! :hugging:`,
+      `hugs ${receivers}! :hugging:`,
+      `gives ${receivers} a warm hug~ :hugging:`,
+      `cozily hugs ${receivers} :hugging:`,
+      `shares a loving hug with ${receivers} :heart:!`,
+      `wraps their arms around ${receivers} for a long, comfy hug! :hugging:`,
+      `warmly hugs ${receivers} :hugging:`,
+      `gives ${receivers} a big hug :hugging:`,
+      `hugs ${receivers} tight :hugging:`,
+      `spreads their arms and locks ${receivers} in a cozy hug! :hugging:`,
+      `smiles and hugs ${receivers}! :hugging:`
+    ];
+
+    const rand = Math.floor(Math.random() * hugs.length);
+    return Promise.resolve(evt.message.author.mention + ` ${hugs[rand]}`);
+  }
+  return Promise.resolve(evt.message.author.mention + ` hugs themselves! :hugging:`);
+}
+
+export default {
+  hug
+};
+
+export const help = {
+  hug: {
+    parameters: ['@User']
+  }
+};
--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -1,24 +1,3 @@
-// import Promise from 'bluebird';
-// import nconf from 'nconf';
-// import R from 'ramda';
-//
-// import { getShardsCmdResults } from '../../redis';
-//
-//
-// function botinfo(client, evt, suffix, lang, json) {
-//   const server_count = {guilds: client.Guilds.length, channels: client.Channels.length, users: client.Users.length};
-//
-//   if (nconf.get('SHARDING') && !json) {
-//     getShardsCmdResults('servers')
-//       .then(R.append({results: server_count}))
-//       .then(R.pluck('results'))
-//       .filter(results => !R.isEmpty(results))
-//       .reduce((sum, res) => R.zipObj(R.keys(res), R.map(key => sum[key] + res[key], R.keys(res))))
-//       .then(res => `Connected to ${res.guilds} servers, ${res.channels} channels and ${res.users} users.`);
-//
-//
-
-
 import Promise from 'bluebird';
 import nconf from 'nconf';
 import R from 'ramda';
@@ -46,41 +25,68 @@ function botinfo(client, evt, suffix, lang, json) {
             icon_url: 'https://cdn.discordapp.com/avatars/174186616422662144/e6b8c266186a60f6b947d1635c09459e.jpg' // eslint-disable-line camelcase
           },
           fields: [
-            { name: 'Owner:',
+            { name: '\uD83D\uDD75\uD83C\uDFFC Owner:',
               value: 'Phun#5241',
               inline: true },
-            { name: 'Library:',
-              value: 'Discordie',
+            { name: '\uD83E\uDD16 Version:',
+              value: 'FurBot 3.8.0',
               inline: true },
-            { name: 'FurBot Version:',
-              value: '3.7.2',
+            { name: '\uD83D\uDCDA Library:',
+              value: 'Discordie^0.11.0',
               inline: true },
-            { name: 'Invite:',
-              value: `Use \`!invite\``,
+            { name: '\uD83D\uDEE1 Guilds:',
+              value: `${res.guilds}`,
               inline: true },
-            { name: 'Donate:',
-              value: `[Click Here](https://paypal.me/phunbun)`,
+            { name: '\uD83D\uDCD4 Channels:',
+              value: `${res.channels}`,
               inline: true },
-            { name: 'FurBot Server:',
+            { name: '\uD83D\uDC65 Users:',
+              value: `${res.users}`,
+              inline: true },
+            { name: '\uD83D\uDCEC Invite:',
+              value: `[Click Here](https://discordapp.com/oauth2/authorize?&client_id=174176308396425217&scope=bot&permissions=268561430)`,
+              inline: true },
+            { name: '\uD83D\uDCB5 Patreon:',
+              value: `[Click Here](https://www.patreon.com/bePatron?u=5578447)`,
+              inline: true },
+            { name: '\uD83D\uDEAA FurBot Server:',
               value: `[Click Here](https://discord.gg/H7W49Ps)`,
-              inline: true },
-            { name: 'Shard Stats:',
-              value: `**Servers:** ${client.Guilds.length}\n**Channels:** ${client.Channels.length}\n**Users:** ${client.Users.length}`,
-              inline: true },
-            { name: 'FurBot Stats:',
-              value: `**Servers:** ${res.guilds}\n**Channels:** ${res.channels}\n**Users:** ${res.users}`,
-              inline: true },
-            { name: 'Shard Uptime:',
-              value: `${uptimeh}h ${uptimem}m ${uptimes}s`,
               inline: true }
           ]
         };
-        return Promise.resolve(evt.message.channel.sendMessage('', false, embed));
-      });
+        let embed2 = {
+          color: 2455143,
+          author: {
+            name: `Shard Info`,
+            icon_url: 'https://cdn.discordapp.com/avatars/174186616422662144/e6b8c266186a60f6b947d1635c09459e.jpg' // eslint-disable-line camelcase
+          },
+          fields: [
+            { name: '\uD83D\uDEE1 Guilds:',
+              value: `${client.Guilds.length}`,
+              inline: true },
+            { name: '\uD83D\uDCD4 Channels:',
+              value: `${client.Channels.length}`,
+              inline: true },
+            { name: '\uD83D\uDC65 Users:',
+              value: `${client.Users.length}`,
+              inline: true },
+            { name: '\uD83D\uDC8E Shard ID:',
+              value: `Shard ${Number(nconf.get('SHARD_NUMBER')+1)} of ${Number(nconf.get('SHARD_COUNT'))}`,
+              inline: true },
+            { name: '\uD83E\uDD16 Shard Uptime:',
+              value: `${uptimeh}h ${uptimem}m ${uptimes}s`,
+              inline: true },
+            { name: '\u200C',
+              value: '\u200C',
+              inline: true }
+          ]
+        };
+        Promise.resolve(evt.message.channel.sendMessage('', false, embed));
+        return Promise.resolve(evt.message.channel.sendMessage('', false, embed2));
+     });
   }
-
   if (json) return Promise.resolve(server_count);
-  return Promise.resolve(`Connected tod ${server_count.guilds} servers, ${server_count.channels} channels and ${server_count.users} users.`);
+  return Promise.resolve(`Connected to ${server_count.guilds} servers, ${server_count.channels} channels and ${server_count.users} users.`);
 }
 
 export const help = {info: {}};
