5c8d53fe382a09aed583c44949af7750bcd47f0c
PhunStyle <jager1993@gmail.com>
2017-05-11 19:04:47 +0200

    Fix haystack lol

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ if (process.env.NODE_ENV === 'development') {
   console.log(`==============================================================================`);
   console.log(chalk.magenta.bold(`[FurBot]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting dev...`);
   require('babel-register');
+  require('babel-polyfill');
   require('./src/');
 } else {
   console.log(`==============================================================================`);
   console.log(chalk.magenta.bold(`[FurBot]`), chalk.blue(`[${moment().format('DD-MM-YYYY HH:mm:ss')}]`), `Booting...`);
+  require('babel-polyfill');
   require('./dist');
 }
