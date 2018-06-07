7c03fad1f3ef01115f55d2099cbad6b5a4b97708
PhunStyle <PhunStyle@users.noreply.github.com>
2018-06-07 14:59:20 +0200

    Heres ur straight flags

--- src/commands/fun/pride.js
+++ src/commands/fun/pride.js
@@ -7,10 +7,13 @@ var gm = require('gm').subClass({imageMagick: true});
 
 function pride(client, evt, suffix, lang) {
   let flagSuffix = suffix.split(' ')[0];
-  let flagArray = ['ace', 'bisexual', 'genderfluid', 'genderqueer', 'lesbian', 'nonbinary', 'pansexual', 'rainbow', 'transgender'];
+  let flagArray = ['ace', 'bisexual', 'genderfluid', 'genderqueer', 'lesbian', 'nonbinary', 'pansexual', 'rainbow', 'straight', 'transgender'];
   let validSuffix = (flagArray.indexOf(flagSuffix) > -1);
+  let doStraight = suffix.includes('straight');
   let doBorder = suffix.includes('border');
   let doRotate = suffix.includes('rotate');
+  let doOverlay = suffix.includes('overlay');
+  let doBackground = suffix.includes('background');
 
   if (!suffix || !validSuffix) {
     return Promise.resolve(`${T('pride_usage', lang)}`);
@@ -22,49 +25,81 @@ function pride(client, evt, suffix, lang) {
 
   if (doRotate) prideFlag += '_rotated';
   if (doBorder) prideFlag += '_border';
-
-  // if (evt.message.attachments.length) {
-  //   if (evt.message.attachments[0].url) {
-  //     imageLink = evt.message.attachments[0].url;
-  //   }
-  // }
+  if (doOverlay) prideFlag += '_overlay';
 
   let fileDir = path.join(__dirname, '../../images/');
   let gayput = fileDir + '/flags/pride_' + prideFlag + '.png';
   let output = fileDir + '/tmp/pride-' + evt.message.author.id + '.png';
 
-  return new Promise((resolve, reject) => {
-    gm(request(imageLink))
-    .resize('236', '236')
-    .crop(236, 236, 0, 0)
-    .write(output, (err, buf) => {
-      if (err) return console.log(err);
-      gm(236, 236, 'none')
+  if (!doOverlay && !doBackground && !doStraight) {
+    return new Promise((resolve, reject) => {
+      gm(request(imageLink))
+      .resize('236', '236')
+      // .crop(236, 236, 0, 0)
+      .write(output, (err, buf) => {
+        if (err) return console.log(err);
+        gm(236, 236, 'none')
         .fill(output)
         .drawCircle((236/2)-1, (236/2)-1, (236/2)-1, 0)
         .write(output, (err, finalbuf) => {
           if (err) return console.log(err);
           gm(gayput)
-            .composite(output)
-            .geometry('+10+10')
-            .toBuffer('PNG', (err, realbuf) => {
-              if (err) return console.log(err);
-              resolve(realbuf);
-            })
+          .composite(output)
+          .geometry('+10+10')
+          .toBuffer('PNG', (err, realbuf) => {
+            if (err) return console.log(err);
+            resolve(realbuf);
+          })
         })
+      });
     });
-  });
+  }
 
-  // return new Promise((resolve, reject) => {
-  //   gm(request(imageLink))
-  //   .out('-resize', '800x800>')
-  //   .out('-liquid-rescale', '50%')
-  //   .out('-liquid-rescale', '150%')
-  //   .toBuffer('PNG', (err, buffer) => {
-  //     if (err) return console.log(err);
-  //     resolve(buffer);
-  //   });
-  // });
+  if (doOverlay || doStraight) {
+    return new Promise((resolve, reject) => {
+      gm(request(imageLink))
+      .resize('256', '256')
+      // .crop(236, 236, 0, 0)
+      .write(output, (err, buf) => {
+        if (err) return console.log(err);
+        gm(256, 256, 'none')
+        .fill(output)
+        .drawCircle((256/2), (256/2)-1, (256/2), 0)
+        .write(output, (err, finalbuf) => {
+          if (err) return console.log(err);
+          gm(output)
+          .composite(gayput)
+          .toBuffer('PNG', (err, realbuf) => {
+            if (err) return console.log(err);
+            resolve(realbuf);
+          })
+        })
+      });
+    });
+  }
+
+  if (doBackground) {
+    return new Promise((resolve, reject) => {
+      gm(request(imageLink))
+      .resize('256', '256')
+      // .crop(236, 236, 0, 0)
+      .write(output, (err, buf) => {
+        if (err) return console.log(err);
+        gm(256, 256, 'none')
+        .fill(output)
+        .drawCircle((256/2), (256/2)-1, (256/2), 0)
+        .write(output, (err, finalbuf) => {
+          if (err) return console.log(err);
+          gm(gayput)
+          .composite(output)
+          .toBuffer('PNG', (err, realbuf) => {
+            if (err) return console.log(err);
+            resolve(realbuf);
+          })
+        })
+      });
+    });
+  }
 }
 
 export default {
