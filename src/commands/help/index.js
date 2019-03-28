d53b63b3411ca78ae9ff21a8833b6651799c72ff
PhunStyle <PhunStyle@users.noreply.github.com>
2019-03-29 00:02:03 +0100

    4.4.6 - Fix Help List

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -3,6 +3,8 @@ import nconf from 'nconf';
 import path from 'path';
 import R from 'ramda';
 
+import { getGuildPrefix } from '../../redis';
+
 import logger from '../../logger';
 import meme from './meme';
 import T from '../../translate';
@@ -43,97 +45,115 @@ if (!process.env.TEST) {
 }
 
 export function subCommands(client, evt, method, lang) {
-  const subcommands = R.sort(R.prop('name'))(help_parameters[method].subcommands);
+  let guildToFind;
+  if (!evt.message.channel.isPrivate) guildToFind = evt.message.guild.id;
+  if (evt.message.channel.isPrivate) guildToFind = '0';
+  getGuildPrefix(guildToFind).then(guildPrefix => {
+    const subcommands = R.sort(R.prop('name'))(help_parameters[method].subcommands);
 
-  let text = R.map(subcommand => {
-    const trans_key = `${method}_${subcommand.name.replace(/ /g, '_')}`;
-    const translation = T(trans_key, lang);
-    if (!translation) return;
+    let text = R.map(subcommand => {
+      const trans_key = `${method}_${subcommand.name.replace(/ /g, '_')}`;
+      const translation = T(trans_key, lang);
+      if (!translation) return;
 
-    const secondary_name = subcommand.secondary_name ? `\` or \`${subcommand.secondary_name}` : '';
-    if (subcommand.parameters && help_parameters[method].prefix !== false) {
-      const parameters = !R.is(String, subcommand.parameters) ? R.join(' ', subcommand.parameters) : subcommand.parameters;
-      return `**\`${nconf.get('PREFIX')}${method}\`** \`${subcommand.name}${secondary_name} \`__\`${parameters}\`__
+      const secondary_name = subcommand.secondary_name ? `\` or \`${subcommand.secondary_name}` : '';
+      if (subcommand.parameters && help_parameters[method].prefix !== false) {
+        const parameters = !R.is(String, subcommand.parameters) ? R.join(' ', subcommand.parameters) : subcommand.parameters;
+        return `**\`${guildPrefix}${method}\`** \`${subcommand.name}${secondary_name} \`__\`${parameters}\`__
+        ${translation}`;
+      }
+      if (subcommand.parameters && help_parameters[method].prefix === false) {
+        return `**\`${guildPrefix}${subcommand.name}\`** \`${R.join(' ', subcommand.parameters)}\`
+        ${translation}`;
+      }
+      if (!subcommand.parameters && help_parameters[method].prefix === false) {
+        return `**\`${guildPrefix}${subcommand.name}\`**
+        ${translation}`;
+      }
+      return `**\`${guildPrefix}${method}\`** \`${subcommand.name}${secondary_name}\`
       ${translation}`;
-    }
-    if (subcommand.parameters && help_parameters[method].prefix === false) {
-      return `**\`${nconf.get('PREFIX')}${subcommand.name}\`** \`${R.join(' ', subcommand.parameters)}\`
-      ${translation}`;
-    }
-    if (!subcommand.parameters && help_parameters[method].prefix === false) {
-      return `**\`${nconf.get('PREFIX')}${subcommand.name}\`**
-      ${translation}`;
-    }
-    return `**\`${nconf.get('PREFIX')}${method}\`** \`${subcommand.name}${secondary_name}\`
-    ${translation}`;
-  }, subcommands);
+    }, subcommands);
 
-  text = R.join('\n', R.reject(R.isNil, text));
-  if (help_parameters[method].header_text) {
-    const header_text = T(help_parameters[method].header_text, lang);
-    text = `${header_text}\n\n${text}`;
-  }
+    text = R.join('\n', R.reject(R.isNil, text));
+    if (help_parameters[method].header_text) {
+      const header_text = T(help_parameters[method].header_text, lang);
+      text = `${header_text}\n\n${text}`;
+    }
 
-  return Promise.resolve(text);
+    return evt.message.channel.sendMessage(text);
+  });
+  return Promise.resolve(true);
 }
 
 // E.g. !help useful
 function helpCategory(client, evt, category, lang = 'en') {
-  let methods;
-  methods = categories[category].sort();
-  let assistText = '';
+  let guildToFind;
+  if (!evt.message.channel.isPrivate) guildToFind = evt.message.guild.id;
+  if (evt.message.channel.isPrivate) guildToFind = '0';
+  getGuildPrefix(guildToFind).then(guildPrefix => {
+    let methods;
+    methods = categories[category].sort();
+    let assistText = '';
 
-  if (category === 'images') {
-    assistText = T('assist_images', lang);
-  }
-
-  const text = R.map(name => {
-    const translation = T(name, lang);
-    if (!translation && category !== 'other' && !R.contains(name, categories.other)) return;
-
-    let parameters;
-    if (help_parameters[name] && help_parameters[name].parameters) {
-      parameters = !R.is(String, help_parameters[name].parameters) ? R.join(' ', help_parameters[name].parameters || []) : help_parameters[name].parameters;
+    if (category === 'images') {
+      assistText = T('assist_images', lang);
     }
-    let text = `**\`${nconf.get('PREFIX')}${name}\`**`;
-    if (parameters) text += ` \`${parameters}\``;
-    if (translation) text += `\n\t\t${translation}`;
 
-    return text;
-  }, methods);
+    const text = R.map(name => {
+      const translation = T(name, lang);
+      if (!translation && category !== 'other' && !R.contains(name, categories.other)) return;
 
-  return Promise.resolve(assistText + R.join('\n', R.reject(R.isNil, text)));
+      let parameters;
+      if (help_parameters[name] && help_parameters[name].parameters) {
+        parameters = !R.is(String, help_parameters[name].parameters) ? R.join(' ', help_parameters[name].parameters || []) : help_parameters[name].parameters;
+      }
+      let text = `**\`${guildPrefix}${name}\`**`;
+      if (parameters) text += ` \`${parameters}\``;
+      if (translation) text += `\n\t\t${translation}`;
+
+      return text;
+    }, methods);
+
+    return evt.message.channel.sendMessage(assistText + R.join('\n', R.reject(R.isNil, text)));
+  });
+  return Promise.resolve(true);
 }
 
 function help(client, evt, suffix, lang) {
-  const category = suffix.toLowerCase();
-  if (categories[category]) return helpCategory(client, evt, category, lang);
-  const help_methods = R.keys(categories).sort();
-  // help_methods.push('all');
-  help_methods.splice(help_methods.indexOf('help'), 1);
+  let guildToFind;
+  if (!evt.message.channel.isPrivate) guildToFind = evt.message.guild.id;
+  if (evt.message.channel.isPrivate) guildToFind = '0';
+  getGuildPrefix(guildToFind).then(guildPrefix => {
+    const category = suffix.toLowerCase();
+    if (categories[category]) return helpCategory(client, evt, category, lang);
+    const help_methods = R.keys(categories).sort();
+    // help_methods.push('all');
+    help_methods.splice(help_methods.indexOf('help'), 1);
 
-  const text = R.map(param => {
-    return `**\`${nconf.get('PREFIX')}help\`** \`${param}\`
-    ${T('help_' + param, lang)}`;
-  }, help_methods);
+    const text = R.map(param => {
+      return `**\`${guildPrefix}help\`** \`${param}\`
+      ${T('help_' + param, lang)}`;
+    }, help_methods);
 
-  // Adds commands that aren't just `help`;
-  R.forEach(name => {
-    const translation = T(name, lang);
-    if (!translation) return;
+    // Adds commands that aren't just `help`;
+    R.forEach(name => {
+      const translation = T(name, lang);
+      if (!translation) return;
 
-    let parameters;
-    if (help_parameters[name]) {
-      parameters = !R.is(String, help_parameters[name].parameters) ? R.join(' ', help_parameters[name].parameters || []) : help_parameters[name].parameters;
-    }
+      let parameters;
+      if (help_parameters[name]) {
+        parameters = !R.is(String, help_parameters[name].parameters) ? R.join(' ', help_parameters[name].parameters || []) : help_parameters[name].parameters;
+      }
 
-    let command_text = `**\`${nconf.get('PREFIX')}${name}\`**`;
-    if (parameters) command_text += ` \`${parameters}\``;
-    if (translation) command_text += `\n\t${translation}`;
-    text.push(command_text);
-  }, categories.help);
+      let command_text = `**\`${guildPrefix}${name}\`**`;
+      if (parameters) command_text += ` \`${parameters}\``;
+      if (translation) command_text += `\n\t${translation}`;
+      text.push(command_text);
+    }, categories.help);
 
-  return Promise.resolve(R.join('\n', text));
+    return evt.message.channel.sendMessage(R.join('\n', text));
+  });
+  return Promise.resolve(true);
 }
 
 function memelist(client, evt, suffix) {
